88b98580e9ce21c87bb55aa86390d2dab648db50
void4d <tulubaevp@icloud.com>
2023-09-24 00:30:43 +0500

    Добавить базовую валидацию на наличие текста в поле на экране входа

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,24 +1,74 @@
-import '../index.css'
-import React from 'react'
+import '../index.css';
+import React from 'react';
 
 function Login({ onLogin }) {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
 
+  const [emailError, setEmailError] = React.useState('');
+  const [passwordError, setPasswordError] = React.useState('');
+  const [isFormValid, setIsFormValid] = React.useState(false);
+
   function handleSubmit(e) {
     e.preventDefault();
-    onLogin(email, password)
+
+    if (!emailError & !passwordError) {
+      onLogin(email, password);
+    }
+  }
+
+  function handleInputEmail(e) {
+    setEmail(e.target.value);
+
+    if (!e.target.value) {
+      setEmailError('Поле не может быть пустым');
+      setIsFormValid(false);
+    } else {
+      setEmailError('');
+      setIsFormValid(true);
+    }
+  }
+
+  function handleInputPassword(e) {
+    setPassword(e.target.value);
+
+    if (!e.target.value) {
+      setPasswordError('Поле не может быть пустым');
+      setIsFormValid(false);
+    } else {
+      setPasswordError('');
+      setIsFormValid(true);
+    }
   }
 
   return (
     <form className="login-register" onSubmit={handleSubmit}>
       <h1 className="login-register__title">Вход</h1>
-      <input className="login-register__input" value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder="Email" type="email" required></input>
-      <input className="login-register__input" value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder="Пароль" type="password" required></input>
-      <button className="login-register__button">Войти</button>
+      <div class="login-register__input-container">
+        <input
+          className="login-register__input"
+          value={email}
+          onChange={handleInputEmail}
+          placeholder="Email"
+          type="email"
+          required
+        ></input>
+        <span className="login-register__error-message">{emailError}</span>
+      </div>
+      <div class="login-register__input-container">
+        <input
+          className="login-register__input"
+          value={password}
+          onChange={handleInputPassword}
+          placeholder="Пароль"
+          type="password"
+          required
+        ></input>
+        <span className="login-register__error-message">{passwordError}</span>
+      </div>
+      <button className={`login-register__button ${!isFormValid ? 'popup__save-button_disabled' : ''}`} disabled={!isFormValid}>Войти</button>
     </form>
-  )
+  );
 }
 
-export default Login
-
+export default Login;
